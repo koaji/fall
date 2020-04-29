c4e9c5f6af84372015afa6a13966df223d681768
koaji <koajisasi23@gmail.com>
2020-04-29 20:59:29 +0900

    移動範囲の調整と落ち物を変更
    バスケットの画面外移動を防止し，落ち物をデバッグ用から本番用に変更．

--- src/main.py
+++ src/main.py
@@ -18,14 +18,24 @@ def main():
     pygame.init()# Pygameの初期化
     screen = pygame.display.set_mode(window_size) # 400*300の画面
     pygame.display.set_caption("Test")            # タイトルバーに表示する文字
-    x0 = 10
-    y0 = 10
-    x1 = 20
-    y1 = 20
     score = 0
 
-    rect = Rect(x0, y0, x1, y1)
-    stage = Rect(10,10,window_size[0] - 20, window_size[1] - 20)
+    stage = Rect(10, 10, window_size[0] - 20, window_size[1] - 20)
+
+    drops_list = []
+    drops_pos = []
+    drops_image = pygame.image.load(r".\..\resource\drops.png").convert_alpha()
+    for i in range(0, 3):
+        temp = pygame.Surface((20,20))
+        temp.blit(drops_image, (0, 0), (i * 20, 0, 20, 20))
+        # なぜかアルファチャンネルが効かないので，透過色キーを設定して，透過処理
+        #temp = pygame.transform.scale(temp, (40, 40))
+        temp.set_colorkey(temp.get_at((0,0)), RLEACCEL)
+        drops_list.append(temp)
+        # アイテム位置を初期化
+        drops_pos.append(temp.get_rect())
+        drops_pos[i].move_ip((random.randrange(0,window_size[0]) - drops_pos[i].right),- drops_pos[i].top)
+
     back_ground_image = pygame.image.load(r".\..\resource\back_ground.png").convert_alpha()
     back_ground = back_ground_image.get_rect()
     basket_image = pygame.image.load(r".\..\resource\basket.png").convert_alpha()
@@ -39,35 +49,37 @@ def main():
         screen.fill((0,0,0))        # 画面を黒色(#000000)に塗りつぶし
         pressed_key = pygame.key.get_pressed()
         if pressed_key[K_LEFT]:
-            basket.move_ip(-1,0)
+            if basket.left > 0 :
+                basket.move_ip(-1,0)
         if pressed_key[K_RIGHT]:
-            basket.move_ip(1, 0)
-        if pressed_key[K_DOWN]:
-            rect.move_ip(random.randrange(window_size[0]), -window_size[1] - rect.top)
-            
-        rect.move_ip(0,1)
-        screen.blit(back_ground_image, back_ground)
-
-        pygame.draw.rect(screen, (0, 80, 0), rect)
-        pygame.draw.rect(screen, (255, 255, 255), stage, 5)
-
-        screen.blit(basket_image, basket)
+            if basket.right <= window_size[0] :
+                basket.move_ip(1, 0)
         
+        screen.blit(back_ground_image, back_ground)
+        pygame.draw.rect(screen, (255, 255, 255), stage, 5)
+        screen.blit(basket_image, basket)
+        screen.blit(drops_list[0], drops_pos[0])
+        screen.blit(drops_list[1], drops_pos[1])
+        screen.blit(drops_list[2], drops_pos[2])
 
         score_text = font.render("SCORE:{0}".format(score), True, (0, 0, 0))
-        screen.blit(score_text, (200, 10))
-        
+        screen.blit(score_text, (180, 10))
+
+        for i in range(0,3):
+            if basket.left <= drops_pos[i].left and basket.right >= drops_pos[i].right:
+                if basket.top + 5 <= drops_pos[i].bottom and basket.top + 5 >= drops_pos[i].top:
+                    drops_pos[i].move_ip((random.randrange(0,window_size[0]) - drops_pos[i].right),- drops_pos[i].top)
+                    print("get!!")
+                    score = score + 1
+                    print(score)
+
+            if drops_pos[i].bottom == window_size[1]:
+                drops_pos[i].move_ip((random.randrange(0, window_size[0]) - drops_pos[i].right), -drops_pos[i].top)
+            
+            drops_pos[i].move_ip(0, 1)
+            
+
         pygame.display.update()  # 画面を更新
-
-        if basket.left <= rect.left and basket.right >= rect.right:
-            if basket.top + 5 <= rect.bottom and basket.top + 5 >= rect.top:
-                rect.move_ip((random.randrange(0,window_size[0]) - rect.right),- rect.top)
-                print("get!!")
-                score = score + 1
-                print(score)
-
-        if rect.bottom == window_size[1]:
-            rect.move_ip((random.randrange(0, window_size[0]) - rect.right), -rect.top)
 
         # イベント処理
         for event in pygame.event.get():
